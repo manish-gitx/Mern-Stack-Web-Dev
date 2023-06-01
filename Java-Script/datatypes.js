0117fd5f79a2db0ab072ad27d1fa6e9ae529ca2d
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-06-01 11:01:24 +0530

    e

---
+++ Java-Script/datatypes.js
@@ -0,0 +1,6 @@
+const Myvar='mehul'
+// arr=['manish',Myvar,true,70]
+// console.log(arr)
+let M
+M=20000000000000000000000000000000n
+console.log(M)
---
+++ Java-Script/first.js
@@ -0,0 +1,8 @@
+console.log('hello world')
+// const Myvar =250
+// console.log(Myvar)
+let Myvar=100
+console.log(Myvar)
+Myvar=101
+console.log(Myvar)
+
